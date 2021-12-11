247d1d213ffc0673e492abc536f69e3925f5e03d
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2021-12-11 18:24:39 +0000

    Set up the template and connect with views and urls the template for the calendar

--- advent_calendar/views.py
+++ advent_calendar/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def advent(request):
+    """ A view to return calendar page """
+
+    return render(request, 'advent_calendar/advent.html')
