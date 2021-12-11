645db206ecfcb3f88e7d2aac6df4e052eec2197f
John Traas <j.j.h.traas@gmail.com>
2021-12-11 19:44:19 +0000

    stashing

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,15 @@
+"""
+All data models for recipes app
+"""
+
 from django_better_admin_arrayfield.models.fields import ArrayField
 from django.db import models
 
 
 class Recipe(models.Model):
+    """
+    Basic recipe model - Will return title on self query
+    """
     title = models.CharField(max_length=255, null=False, blank=False)
     description = models.CharField(max_length=500, null=True, blank=True)
     author = models.CharField(max_length=255, null=True, blank=True)
@@ -11,4 +18,5 @@ class Recipe(models.Model):
     method = ArrayField(models.CharField(max_length=200, blank=True))  # noqa: E501
 
     def __str__(self):
+        """ Self Init """
         return self.title
