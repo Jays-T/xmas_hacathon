ebb676442343e8549a7ad0d125002a34244a24b2
John Traas <j.j.h.traas@gmail.com>
2021-12-12 20:22:28 +0000

    stashing

--- northpole/views.py
+++ northpole/views.py
@@ -7,5 +7,8 @@ def index(request):
     if not request.user.is_authenticated:
         messages.warning(request, f"Santa's elves are hangry,\
                          can you feed them in time to save Christmas?")
+        messages.success(request, f"A success message!")
+        messages.info(request, f"An info message!")
+        messages.error(request, f"An error message!")
 
     return render(request, 'northpole/index.html')
