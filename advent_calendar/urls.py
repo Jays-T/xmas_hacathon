7ee6bc6a1ea939f3ca85666469f20335769cd3bb
John Traas <j.j.h.traas@gmail.com>
2021-12-13 08:16:10 +0000

    ADD done boolean field to Day model - Users can now set a recipe as completed

--- advent_calendar/urls.py
+++ advent_calendar/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.advent, name='calendar'),
     path('create-calendar/', views.create_advent, name='create_calendar'),
     path('naughty/', views.naughty, name='naughty'),
+    path('mark-complete/<int:day_id>/<calendar_id>', views.set_day_recipe_complete, name='mark_complete')
 ]
