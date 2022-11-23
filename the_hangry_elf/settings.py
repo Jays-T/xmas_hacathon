b834064a58cb378f29b69421bb9fdc7d477c3357
John Traas <j.j.h.traas@gmail.com>
2022-11-23 11:00:02 +0000

    deploy to render

--- the_hangry_elf/settings.py
+++ the_hangry_elf/settings.py
@@ -34,6 +34,10 @@ DEBUG = False
 
 
 ALLOWED_HOSTS = [os.environ.get('HEROKU_URL'), 'localhost']
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -61,6 +65,7 @@ INSTALLED_APPS = [
     'profiles',
     'recipes',
     'advent_calendar',
+    'countdown',
 ]
 
 MIDDLEWARE = [
